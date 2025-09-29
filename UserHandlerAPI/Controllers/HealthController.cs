a09a9432658cbe111d94c4227866568ed7cb3222
marianotravella <marianotravella2@gmail.com>
2025-09-29 17:32:05 -0300

    HealthController

---
+++ UserHandlerAPI/Controllers/HealthController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MyAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HealthController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Ping()
+        {
+            return Ok("The API is running successfully");
+        }
+    }
+}
